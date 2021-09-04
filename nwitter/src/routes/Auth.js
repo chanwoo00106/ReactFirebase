ad40ce28d772214e68dc57cb7dbfedc0f51e7394
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-04 21:21:43 +0900

    로그인 화면 구성

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // 이런 코드가 있으면 알려달라고 노마드형
+    const onChagne = (e) => {
+        const {
+            target: {name, value}
+        } = e;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = e => e.preventDefault();
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                required // 빈 값이 들어오지 못하게 한다
+                value={email}
+                onChange={onChagne}
+                />
+                <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChagne}
+                />
+                <input type="submit" value="Log In" />
+            </form>
+        </div>
+    );
+};
 
 export default Auth;

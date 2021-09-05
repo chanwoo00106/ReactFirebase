7a631b463d59c43b50311b863b8d03d32cfd5c76
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-05 14:28:04 +0900

    Update Home.js
    데이터 받기!!

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
-import { collection, addDoc } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from 'fbase';
 
 const Home = () => {
     const [nweet, setNweet] = useState('');
+    const [nweets, setNweets] = useState([]);
+
+    
+
+    useEffect(() => {
+        const getNweets = async () => {
+            const dbNweets = await getDocs(collection(db, "nweets"));
+            dbNweets.forEach((doc) => {
+                const newObj = {
+                    ...doc.data(),
+                    id: doc.id
+                }
+                setNweets(prev => [newObj, ...prev]);
+            });
+        }
+        getNweets();
+    }, []);
     const onSubmit = async e => {
         e.preventDefault();
         try {
-            await addDoc(collection(db, 'nweets'), {
+            await addDoc(collection(db, "nweets"), {
                 nweet,
                 createdAt: Date().slice(0, 15)
             });
@@ -23,6 +40,13 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" max={120} />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet, i) => (
+                    <div key={i}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
